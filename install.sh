ac14cb33da3dce01f7bda91674c4060273875de0
luckapi <152237316+luckapi@users.noreply.github.com>
2026-06-20 10:39:38 +0800

    Update install.sh
    更新下载地址

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ DEFAULT_ACTION="install"
 DEFAULT_RELEASE_VERSION="latest"
 DEFAULT_LOG_LEVEL="info"
 DEFAULT_KERNEL_LOG_LEVEL="warn"
-DEFAULT_DOWNLOAD_BASE="https://github.com/cedar2025/xboard-node/releases"
+DEFAULT_DOWNLOAD_BASE="https://github.com/luckapi/xboard-node/releases"
 
 ACTION="${DEFAULT_ACTION}"
 MODE=""
